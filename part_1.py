7b6a429108ee3774f2b59c1bc1742b4d6360737c
tp19-beep <tp19@kent.ac.uk>
2026-02-09 13:02:34 +0000

    p 1 and 2

--- part_1.py
+++ part_1.py
@@ -1,1 +1,5 @@
+sample_bay = ["Basalt", "Silica", "Iron", "Dust"]
 
+print(sample_bay.index("Basalt"))
+print(sample_bay.index("Dust"))
+print(len(sample_bay))
--- part_2.py
+++ part_2.py
@@ -1,1 +1,4 @@
+rover_status= [{"Battery": 100}, {"Heater": "Off"}, {"Camera": "Standby"}]
 
+for rover_status in rover_status: 
+    (print(int(rover_status[" "])))
